d1c772e3a0d10ae583318da8abf42315d59a8ce5
Nhật Quân <80318291+quanyb@users.noreply.github.com>
2026-04-10 00:49:29 +0700

    Create IT1108_Bai1_1.c

---
+++ Code Hustack/IT1108_Bai1_1.c
@@ -0,0 +1,12 @@
+// IT1108_Bai1_1_Cộng hai số
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+
+using namespace std;
+
+signed main() {
+    float a, b; cin >> a >> b;
+    cout << setprecision(6) << fixed << a + b;
+    return 0;
+}
